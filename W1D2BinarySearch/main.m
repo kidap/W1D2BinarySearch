2aaa79983655f7eb71c325f050bb35ab6fc97511
Karlo Pagtakhan <kidap@yahoo.com>
2016-03-08 17:17:08 -0500

    added comments to the Binary Search Part 1

--- W1D2BinarySearch/main.m
+++ W1D2BinarySearch/main.m
@@ -8,22 +8,33 @@
 
 #import <Foundation/Foundation.h>
 
-//Binary search Part 1
+//-----------------------------------------------------
+// Binary search Part 1
+//-----------------------------------------------------
 NSMutableArray *searchNumber(NSArray *array, int findNumber){
   NSMutableArray *resultIndices = [[NSMutableArray alloc] init];
   int index = 0;
   
+  //Loop at all the numbers
   for (NSString * number in array){
     int numberAtIndex = 0;
     
+    // Get int value of NSNumber
     numberAtIndex = [number intValue];
     
+    //Check if the number equal to the current number
     if (numberAtIndex == findNumber){
+      //Add the number to the result array
       [resultIndices addObject:[NSNumber numberWithInt:index]];
     }
+    
+    //Increment counter to monitor the index
     index ++;
   }
   
+  if ([resultIndices count] == 0){
+    [resultIndices addObject:[NSNumber numberWithInt:-1]];
+  }
   return resultIndices;
 }
 
@@ -31,12 +42,17 @@ NSMutableArray *searchNumber(NSArray *array, int findNumber){
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
 
+    //-----------------------------------------------------
+    // Binary search Part 1
+    //-----------------------------------------------------
     NSArray *sortedArray = @[@2, @3, @3, @5, @6, @6];
     NSMutableArray * resultArray = [[NSMutableArray alloc] init];
     int findNumber = 3;
     
+    //Get the index
     resultArray = searchNumber(sortedArray, findNumber);
     
+    //Print out
     NSLog(@"The index of the number %d is %@", findNumber, resultArray );
     
     
